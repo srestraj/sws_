4cd291a43bf242922ece9902e11d4dd48adc4da9
Yagya Adh <yagyaadhikari02@gmail.com>
2024-09-14 16:04:27 +0545

    changes 4:04

--- sws_/src/components/Nav/NavBarTabs.jsx
+++ sws_/src/components/Nav/NavBarTabs.jsx
@@ -3,26 +3,48 @@ import { NavLink } from "react-router-dom";
 import logo from "../../assets/iafgg.svg";
 
 const navLink = [
-  { id: 1, linkTitile: "Home", navPath: "/" },
-  { id: 2, linkTitile: "Discover", navPath: "discover" },
-  { id: 3, linkTitile: "Education", navPath: "education" },
-  { id: 4, linkTitile: "Science", navPath: "science" },
-  { id: 5, linkTitile: "About the IAFGG", navPath: "about-us" },
-  { id: 6, linkTitile: "Events", navPath: "events" },
-  { id: 7, linkTitile: "Shop", navPath: "shop" },
+  { id: 1, linkTitile: "Home", navPath: "/", slug: "" },
+  {
+    id: 2,
+    linkTitile: "Discover",
+    navPath: "discover",
+    slug: "Practical guidance about Orthotropics.",
+  },
+  {
+    id: 3,
+    linkTitile: "Education",
+    navPath: "education",
+    slug: "Educational information about Orthotropics.",
+  },
+  {
+    id: 4,
+    linkTitile: "Science",
+    navPath: "science",
+    slug: "Following the data on Orthotropics.",
+  },
+  {
+    id: 5,
+    linkTitile: "About the IAFGG",
+    navPath: "about-us",
+    slug: "Practical guidance about Orthotropics.",
+  },
+  { id: 6, linkTitile: "Events", navPath: "events", slug: "" },
+  { id: 7, linkTitile: "Shop", navPath: "shop", slug: "" },
 ];
 const NavBarTabs = () => {
   return (
     <>
-      <div className="flex text-sm font-inter relative">
+      <div className="flex flex-col text-sm font-inter relative">
         <ul className="sticky flex ms-10 ">
           {navLink?.map((linksName) => (
-            <li
-              className={` mx-1 border-t-4 border-transparent hover:border-[#2E9BB2] p-2 transition-all`}
-              key={linksName.id}
-            >
-              <NavLink to={linksName.navPath}>{linksName.linkTitile}</NavLink>
-            </li>
+            <div className="flex flex-col">
+              <li
+                className={` mx-1 border-t-4 border-transparent hover:border-[#2E9BB2] p-2 transition-all`}
+                key={linksName.id}
+              >
+                <NavLink to={linksName.navPath}>{linksName.linkTitile}</NavLink>
+              </li>
+            </div>
           ))}
         </ul>
 
