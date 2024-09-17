95f188d9421e986d8c43183bfc819e6ec406ab8a
Yagya Adh <yagyaadhikari02@gmail.com>
2024-09-17 13:03:52 +0545

    1:03 pm changes nesw

--- sws_/src/app/components/finders/FindersCard.jsx
+++ sws_/src/app/components/finders/FindersCard.jsx
@@ -6,12 +6,12 @@ const FindersCard = ({ background }) => {
   if (background == "#27606D") {
     return (
       <div
-        className={`max-w-screen-2xl px-10 mx-auto py-16 relative overflow-hidden mt-10 z-10 rounded-2xl flex justify-evenly items-center bg-['${background}']`}
+        className={`bg-light-blue30 max-w-screen-2xl px-10 mx-20 py-16 relative overflow-hidden mt-10 z-10 rounded-2xl flex flex-wrap justify-evenly items-center bg-['${background}']`}
       >
-        <h1 className="text-light-white10  font-sans font-bold text-3xl text-start w-2/3 ">
+        <h1 className="text-light-white10 font-sans font-bold text-3xl text-start w-2/3 my-2">
           Join the International Association of <br /> Facial Growth Guidance
         </h1>
-        <div>
+        <div className="mt-3">
           <PillButton
             title={"Become a member"}
             backgroundColor={"bg-light-yellow20"}
@@ -20,21 +20,19 @@ const FindersCard = ({ background }) => {
         <img
           src={darkLogo}
           alt="logo_img"
-          // className="w-full absolute min-w-fit h-fit -z-20 opacity- "
-          className="w-full  absolute min-h-fit min-w-fit  -z-10  opacity-85"
+          className="w-full absolute min-h-fit min-w-fit -z-10  opacity-45"
         />
       </div>
     );
   }
-  // background: ;
 
   if (background == "#E3E9ED") {
     return (
       <>
         <div
-          className={`max-w-screen-2xl px-10 mx-auto py-16 relative overflow-hidden mb-10 z-10 rounded-2xl flex justify-evenly items-center bg-['${background}`}
+          className={`max-w-screen-2xl px-10 border mx-20 py-16 relative overflow-hidden mb-10 z-10 rounded-2xl flex justify-evenly items-center bg-['${background}`}
         >
-          <h1 className="text-light-gray20 font-sans font-bold text-3xl text-start w-2/3">
+          <h1 className="text-light-gray20 font-sans font-bold text-3xl text-start w-2/3 my-2">
             Find an Orthotropics practitioner <br /> near you
           </h1>
           <PillButton
